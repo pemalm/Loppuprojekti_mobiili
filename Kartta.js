e41c329564017e08e80fde0304b5894ad67bc900
pemalm <62079499+pemalm@users.noreply.github.com>
2021-03-14 11:19:06 +0200

    Loppuprojekti

---
+++ Kartta.js
@@ -0,0 +1,153 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Alert,
+  Linking, TouchableOpacity
+} from 'react-native';
+import MapView, { Callout, Marker } from 'react-native-maps';
+import Constants from 'expo-constants';
+
+export default function App() {
+  const [sijainti, setSijainti] = useState({
+    lat: 60.166640739,
+    lng: 24.943536799,
+  });
+  const [ravintola, setRavintola] = useState();
+
+  async function getRavintola() {
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=helsinki,finland&key=AIzaSyCjyREvhASQpXGViuOX3ND66JQRSQLPlPU'`;
+    try {
+      const fetched = await fetch(url);
+      const fetchedJson = await fetched.json();
+      const url2 = `http://open-api.myhelsinki.fi/v1/places/?tags_search=Restaurant&distance_filter=60.166640739%2C24.943536799%2C2`;
+      const haetutRavintolat = await fetch(url2);
+      const haetutJSON = await haetutRavintolat.json();
+      setRavintola(haetutJSON.data);
+    } catch (e) {
+      <Text></Text>;
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text
+        style={{
+          margin: 10,
+          fontWeight: 'bold',
+          color: '#000000',
+          fontSize: 15,
+        }}>
+        {' '}
+        Eikö ruoka onnistunut? Etsi lähin ravintola!
+      </Text>
+      <MapView
+        style={styles.kartta}
+        region={{
+          latitude: sijainti.lat,
+          longitude: sijainti.lng,
+          latitudeDelta: 0.0667,
+          longitudeDelta: 0.0667,
+        }}>
+        {ravintola ? (
+          ravintola.map((tiedot) => (
+            <Marker
+              key={tiedot.id}
+              coordinate={{
+                latitude: tiedot.location.lat,
+                longitude: tiedot.location.lon,
+              }}>
+              <Callout
+                tooltip={false}
+                onPress={() => {
+                  Linking.openURL(tiedot.info_url);
+                }}>
+                <Text style={{ fontWeight: 'bold', fontSize: 14 }}>
+                  {tiedot.name.fi}
+                </Text>
+                <Text
+                  style={{
+                    width: 200,
+                    margin: 1,
+                    color: '#000000',
+                    fontSize: 12,
+                  }}>
+                  Ravintolan tiedot:{'\n'}
+                  {tiedot.description.body}{'\n'}
+                  Osoite:{'\n'}
+                  {tiedot.location.address.street_address}
+                </Text>
+                <Text style={{ color: '#FF904D', fontSize: 12 }}>
+                  {'\n'}Ravintolan nettiosoite:
+                </Text>
+                <Text style={{ color: '#FF904D', fontSize: 12 }}>
+                  {tiedot.info_url}
+                </Text>
+                <Text style={{ color: '#000000', fontSize: 10 }}>Klikkaa lisätietoja</Text>
+              </Callout>
+            </Marker>
+          ))
+        ) : (
+          <></>
+        )}
+      </MapView>
+      <View style={{ flexDirection: 'row', padding: 4, alignItems: 'center' }}>
+        <TouchableOpacity style={styles.painike} onPress={() => getRavintola()}>
+          <Text style={styles.nappi}>Etsi lähimmät ravintolat</Text>
+        </TouchableOpacity>
+      </View>
+      <Text>{'\n'}</Text>
+      <Text
+        style={{
+          margin: 10,
+          fontWeight: 'bold',
+          color: '#000000',
+          fontSize: 10,
+        }}>
+        huom! tämä on demo, haku ainoastaan Helsingin alueella
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 30,
+    left: 10,
+    bottom: 20,
+    right: 10,
+    borderWidth: 5,
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#FF904D',
+    padding: 8,
+    alignItems: 'center',
+    borderColor: '#FFFFFF',
+  },
+  kartta: {
+    width: '100%',
+    height: '85%',
+  },
+  painike: {
+    margin: 8,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#000000',
+    borderRadius: 5,
+    backgroundColor: 'green',
+    borderWidth: 1,
+    height: 40,
+    width: 150,
+  },
+  nappi: {
+    color: '#ffffff',
+  },
+});
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Loppuprojekti_mobiili
+# Mobiiliohjelmointi lopputyö, "reseptinhaku" 
+
+Ohjelma tehty React Nativella.
+
+## Ohjelmalla voi
+
+- Hakea reseptejä Recipepuppy palvelusta, ja avata alkuperäiset reseptit selaimeen
+- Tallentaa omia reseptejä muistio-tyyppisesti
+- Ottaa kuvat annoksista
+- Voit listata lähimmät ravintolat, sekä avata ulkoisen karttaohjelman osoitteen löytämistä varten
+
+## Toteutus
+
+- Yhtenäinen käyttöliittymä käyttäen Stack Navigatoria ja yhteneväistä ikoni- ja värimaailmaa
+- Kamera tehty käyttäen expo-camera:a, ja varsinaisen kuvan ottaminen captureScreen kirjastolla. Tämä sen takia että kuviin saadaan teeman mukaiset kehykset.
+- Lähimmät ravintolat listataan käyttäen Googlen nearby-search komentoa, sijainti haetaan automaattisesti käytettävän laitteen gps:n avulla (expo-location)
+
+## Rajapinnat, tekniikkaa ym
+
+- Reseptihaussa recipepuppy API
+- Ravintolahaussa Google maps nearbysearch API
+- Kamera Expo-Camera, käyttäjälupa pyydetään kameralle sekä laitteen sisäisen muistin käytölle
+- Karttahaussa Google Maps API sekä ravintolatiedot open-api.myhelsinki (https://hri.fi/data/dataset/myhelsinki-open-api-paikat-tapahtumat-ja-aktiviteetit). Käytetty eri API lähteitä, jolla saatu erilaisia hakuja tehtyä koodiin. Open-api antaa myös helposti käytettävää dataa karttapalvelua varten.
+- Muistio käyttää SQLite open databasea
+- Ohjelmassa käytetty erilaisia tapoja toteuttaa painikkeet ja muut käyttäjävalinnat, Button, TouchableOpacity, suoraan text-komponenttiin liitetty onPress
